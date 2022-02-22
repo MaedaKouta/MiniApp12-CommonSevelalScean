f6055413bca4075cb3948b1e39f467259d4cce83
MaedaKouta <maedakoutaa@gmail.com>
2022-02-22 15:48:21 +0900

    commit

--- MiniApp12-CommonSevelalScean/ViewController.swift
+++ MiniApp12-CommonSevelalScean/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+        switch segue.identifier ?? "" {
+        case "GreenSegue":
+            (segue.destination as? SecondViewController)?.mode = .ModeGreen
+        case "RedSegue":
+            (segue.destination as? SecondViewController)?.mode = .ModeRed
+        case "BlueSegue":
+            (segue.destination as? SecondViewController)?.mode = .ModeBlue
+        default:
+            break
+            
+        }
     }
-
-
 }
 
